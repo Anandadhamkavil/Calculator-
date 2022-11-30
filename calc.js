c5fa5ede5a6292afe30b35abd6594a6ec48cc9ca
Anandadhamkavil <anandadhamkavil123@gmail.com>
2022-11-30 14:57:40 +0530

    Calculator

---
+++ calc.js
@@ -0,0 +1,12 @@
+function displayValue(num){
+    result.value+=num
+}
+function allclear(){
+    result.value =""
+}
+function evaluateOut(){
+    result.value =eval(result.value)
+}
+function back(){
+    result.value =result.value.slice(0,-1)
+}
